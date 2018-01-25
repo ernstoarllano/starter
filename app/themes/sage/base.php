0192f22a30c3a985e91feeff4bd779521eb5b456
Ernesto Arellano Jr <ernesto@Ernestos-MBP.fios-router.home>
2018-01-24 22:03:55 -0800

    update gitignore, add custom nav walker, update base alert

--- app/themes/sage/base.php
+++ app/themes/sage/base.php
@@ -8,7 +8,7 @@ use Roots\Sage\Wrapper;
 <?php get_template_part('templates/head'); ?>
 <body <?php body_class(); ?>>
     <!--[if IE]>
-        <div class="c-alert c-alert--warning">
+        <div class="alert alert--warning">
             <?php _e('You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.', 'sage'); ?>
         </div>
     <![endif]-->
